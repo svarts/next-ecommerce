531aa4a265135d833c22e8a2e12cafbc7e62253a
svartse <killbillbeatrixx@gmail.com>
2023-08-09 13:26:55 +0300

    config

---
+++ components/Hero.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <div className='hero'>
+            <div className='flex-1 pt-36 padding-x'>
+                <h1 className='hero__title'>
+                    FutureTech Store: Explore the Latest Gadgets and Gizmos!
+                </h1>
+
+                <p>
+                    Explore FutureTech Store for the latest gadgets and embrace innovation today!
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
