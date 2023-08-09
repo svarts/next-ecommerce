558df7213110b599fe240f52ae02bb94191a5a06
svartse <killbillbeatrixx@gmail.com>
2023-08-09 14:36:51 +0300

    type declarations

---
+++ components/CustomButton.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { CustomButtonProps } from "@/types";
+
+function CustomButton( { title, containerStyles, handleClick}: CustomButtonProps ) {
+    return (
+        <button 
+            disabled={ false }
+            type={ "button" }
+            className={`custom-btn`}
+            onClick={() => {}}
+        >
+            <span className={`flex-1`}>
+                title
+            </span>
+        </button>
+    )
+}
+
+export default CustomButton
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+"use client";
+import CustomButton from './CustomButton';
 
 function Hero() {
+    const handleScroll = () => { }
     return (
         <div className='hero'>
             <div className='flex-1 pt-36 padding-x'>
@@ -8,9 +10,15 @@ function Hero() {
                     FutureTech Store: Explore the Latest Gadgets and Gizmos!
                 </h1>
 
-                <p>
+                <p className='hero__subtitle'>
                     Explore FutureTech Store for the latest gadgets and embrace innovation today!
                 </p>
+
+                <CustomButton
+                    title="Explore"
+                    containerStyles="bg-neutral-900 text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
             </div>
         </div>
     )
