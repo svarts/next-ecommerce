c454cd6c197a20d83f74fa1a9624a66ec3692d4c
svartse <killbillbeatrixx@gmail.com>
2023-08-10 14:28:21 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -19,7 +19,16 @@ const Footer = () => {
                 <div className="footer__links">
                     {footerLinks.map((link) => (
                         <div key={link.title} className="footer__link">
-                            <h3>{link.title}</h3>
+                            <h3 className="font-bold">{link.title}</h3>
+                            {link.links.map((item) => (
+                                <Link
+                                key={item.title}
+                                href={item.url} 
+                                className="text-gray-50"
+                                >
+                                    {item.title}
+                                </Link> 
+                            ))}
                         </div>
                     ))}
                 </div>
