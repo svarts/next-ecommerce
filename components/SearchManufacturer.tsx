d3bc5c1d9b4c71d87b664fa348e7fd5cb91775b9
svartse <killbillbeatrixx@gmail.com>
2023-08-10 19:40:39 +0300

    combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,9 +1,48 @@
+"use client";
+import { useState, Fragment } from 'react';
+import  Image  from 'next/image';
 import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+import { Combobox, Transition } from '@headlessui/react'
 
 const SearchManifacturer = ({manufacturer, setManifacturer}: SearchManufacturerProps ) => {
+    function setQuery(value: string): void {
+        throw new Error('Function not implemented.');
+    }
+
     return (
-        <div>SearchManifacturer</div>
+        <div className='search-manufacturer'>
+            <Combobox>
+                <div className='relative w-full'>
+                    <Combobox.Button className="absolute top-[14px]">
+                        <Image 
+                            src="/brand.png"
+                            width={22}
+                            height={22}
+                            className="ml-4"
+                            alt='brand'
+                        />
+                    </Combobox.Button>
+                    <Combobox.Input 
+                        className="search-manufacturer__input"
+                        placeholder='Iphone'
+                        displayValue={(manufacturer: string) => manufacturer}
+                        onChange={(e) => setQuery(e.target.value)}
+                    />
+
+                    <Transition 
+                        as={Fragment}
+                        leave="transition ease-in duration-100"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                        afterLeave={() => setQuery('')}
+                    >
+                        <Combobox.Options>
+                            
+                        </Combobox.Options>
+                    </Transition>
+                </div>
+            </Combobox>
+        </div>
     )
 }
 
