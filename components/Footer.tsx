218e168602a24c4856174b79ed4d2025a488179c
svartse <killbillbeatrixx@gmail.com>
2023-08-10 14:49:22 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,39 +2,50 @@ import Image from "next/image";
 import Link from "next/link";
 import { footerLinks } from "@/constants";
 
-const Footer = () => {
-    return (
-        <footer className="flex flex-col text-white mt-5 border-t border-gray-100">
-            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-                <div className="flex flex-col justify-start items-start gap-6">
-                    <Image
-                    src="/nlogo.svg"
-                    alt="logo"
-                    width={118}
-                    height={18}
-                    className="object-contain"
-                    />
-                    <p className="text-base text-white">FutureTech 2023 <br /> All rights reverved &copy; </p>
-                </div>
-                <div className="footer__links">
-                    {footerLinks.map((link) => (
-                        <div key={link.title} className="footer__link">
-                            <h3 className="font-bold">{link.title}</h3>
-                            {link.links.map((item) => (
+const Footer = () => (
+    <footer className='flex flex-col text-gray-100 mt-5 border-t border-gray-100'>
+        <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+            <div className='flex flex-col justify-start items-start gap-6'>
+                <Image src='/nlogo.svg' alt='logo' width={118} height={18} className='object-contain' />
+                <p className='text-base text-gray-500'>
+                    FutureTech 2023 <br />
+                    All Rights Reserved &copy;
+                </p>
+            </div>
+
+            <div className="footer__links">
+                {footerLinks.map((item) => (
+                    <div key={item.title} className="footer__link">
+                        <h3 className="font-bold">{item.title}</h3>
+                        <div className="flex flex-col gap-5">
+                            {item.links.map((link) => (
                                 <Link
-                                key={item.title}
-                                href={item.url} 
-                                className="text-gray-50"
+                                    key={link.title}
+                                    href={link.url}
+                                    className="text-gray-500"
                                 >
-                                    {item.title}
-                                </Link> 
+                                    {link.title}
+                                </Link>
                             ))}
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-        </footer>
-    )
-}
+        </div>
 
-export default Footer
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t-[0.5px] border-gray-500 sm:px-16 px-6 py-10'>
+            <p>@2023 CarHub. All rights reserved</p>
+
+            <div className="footer__copyrights-link">
+                <Link href="/" className="text-gray-500">
+                    Privacy & Policy
+                </Link>
+                <Link href="/" className="text-gray-500">
+                    Terms & Condition
+                </Link>
+            </div>
+        </div>
+    </footer>
+);
+
+export default Footer;
