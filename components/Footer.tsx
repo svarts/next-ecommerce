54cd9aa4171b534ea487654b9281626a6f8f6c85
svartse <killbillbeatrixx@gmail.com>
2023-08-10 14:51:25 +0300

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => (
         <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
             <div className='flex flex-col justify-start items-start gap-6'>
                 <Image src='/nlogo.svg' alt='logo' width={118} height={18} className='object-contain' />
-                <p className='text-base text-gray-500'>
+                <p className='text-base text-gray-400'>
                     FutureTech 2023 <br />
                     All Rights Reserved &copy;
                 </p>
@@ -22,7 +22,7 @@ const Footer = () => (
                                 <Link
                                     key={link.title}
                                     href={link.url}
-                                    className="text-gray-500"
+                                    className="text-gray-400"
                                 >
                                     {link.title}
                                 </Link>
@@ -33,14 +33,14 @@ const Footer = () => (
             </div>
         </div>
 
-        <div className='flex justify-between items-center flex-wrap mt-10 border-t-[0.5px] border-gray-500 sm:px-16 px-6 py-10'>
-            <p>@2023 CarHub. All rights reserved</p>
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t-[0.5px] border-gray-400 sm:px-16 px-6 py-10'>
+            <p>@2023 FutureTech. All rights reserved</p>
 
             <div className="footer__copyrights-link">
-                <Link href="/" className="text-gray-500">
+                <Link href="/" className="text-gray-400">
                     Privacy & Policy
                 </Link>
-                <Link href="/" className="text-gray-500">
+                <Link href="/" className="text-gray-400">
                     Terms & Condition
                 </Link>
             </div>
