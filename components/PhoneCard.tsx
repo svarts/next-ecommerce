166809a587a312d26c5adac2c8446a655d1fb8d5
svartse <killbillbeatrixx@gmail.com>
2023-08-22 21:42:05 +0300

    phonecard

--- components/PhoneCard.tsx
+++ components/PhoneCard.tsx
@@ -13,7 +13,19 @@ const PhoneCard = ({ phone }: PhoneCardProps) => {
     return (
         <div className='car-card group'>
             <div className='car-car__content'>
-                <img src={image} className="mb-10" width={150} height={150} alt="data-image"/>
+                <img src={image} className="mb-10" width={150} height={150} alt="data-image" />
+                <div className='relative flex w-full mt-2'>
+                    <div className='flex w-full justify-between text-gray'>
+                        <div className='flex flex-col justify-center items-center gap-2' >
+                            <Image
+                                src="/battery.svg" alt='battery'
+                                width={40}
+                                height={40}
+                                className='hover:text-black cursor-pointer active:translate-x-1'
+                            />
+                        </div>
+                    </div>
+                </div>
                 <h2 className='car-card__content-title '>
                     {title}
                 </h2>
@@ -23,13 +35,6 @@ const PhoneCard = ({ phone }: PhoneCardProps) => {
                     {price}
                 </span>
             </p>
-            <div className='relative flex w-full mt-2'>
-                <div className='flex group-hover:invisible w-full justify-between text-gray'>
-                    <div className='' >
-                    
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
