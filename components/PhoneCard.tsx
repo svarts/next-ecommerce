b130b314ad1a5048f66022bc8d07bfc7778dad24
svartse <killbillbeatrixx@gmail.com>
2023-08-23 12:47:01 +0300

    phonecard

--- components/PhoneCard.tsx
+++ components/PhoneCard.tsx
@@ -9,28 +9,40 @@ interface PhoneCardProps {
 }
 
 const PhoneCard = ({ phone }: PhoneCardProps) => {
-    const { image, title, price, year } = phone;
+    const { image, title, price, year, description } = phone;
     return (
-        <div className='car-card group'>
-            <div className='car-car__content'>
-                <img src={image} className="mb-10" width={150} height={150} alt="data-image" />
-                <div className='relative flex w-full mt-2'>
+        <div className='car-card group cursor-pointer'>
+            <div className='relative flex w-full mt-2'>
                     <div className='flex w-full justify-between text-gray'>
                         <div className='flex flex-col justify-center items-center gap-2' >
                             <Image
                                 src="/battery.svg" alt='battery'
-                                width={40}
-                                height={40}
+                                width={35}
+                                height={35}
+                                className='hover:text-black cursor-pointer active:translate-x-1'
+                            />
+                            <Image
+                                src="/brand.svg" alt='battery'
+                                width={35}
+                                height={35}
+                                className='hover:text-black cursor-pointer active:translate-x-1'
+                            />
+                            <Image
+                                src="/storage.svg" alt='battery'
+                                width={35}
+                                height={35}
                                 className='hover:text-black cursor-pointer active:translate-x-1'
                             />
                         </div>
                     </div>
                 </div>
+            <div className='car-car__content'>
+                <img src={image} className="mb-10" width={150} height={150} alt="data-image" />
                 <h2 className='car-card__content-title '>
                     {title}
                 </h2>
             </div>
-            <p className='flex mt-6 text-[32px] font-extrabold'>
+            <p className='flex mt-6 text-[32px] font-normal'>
                 <span>
                     {price}
                 </span>
--- types/index.ts
+++ types/index.ts
@@ -18,4 +18,5 @@ export interface PhoneProps {
     title:string;
     price:number;
     year:number; 
+    description: string;
 }
